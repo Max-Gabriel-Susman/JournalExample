fcc2821b37964b88845db1ea7eae72540fb58c43
Moses <gabe.susman@gmail.com>
2020-04-17 06:35:15 -0600

    Initial Commit

---
+++ Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journal
+//
+//  Created by Moses on 4/17/20.
+//  Copyright © 2020 Max Susman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
